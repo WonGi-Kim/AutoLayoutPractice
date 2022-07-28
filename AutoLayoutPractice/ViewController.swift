a8e8cff4b895ad8559638e1b343a0a64eb6abe3d
WonGi-Kim <kwg970310@gmail.com>
2022-07-28 23:13:46 +0900

    Initial Commit

---
+++ AutoLayoutPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutPractice
+//
+//  Created by 김원기 on 2022/07/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
