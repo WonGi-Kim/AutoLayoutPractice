a20e88a8da0e14fea350f922ccea652cee48170b
WonGi-Kim <kwg970310@gmail.com>
2022-07-29 18:21:20 +0900

    IBoutlet, IBaction 기능

--- AutoLayoutPractice/ViewController.swift
+++ AutoLayoutPractice/ViewController.swift
@@ -8,7 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBAction func tapChangeColorButton(_ sender: UIButton) {
+        //색상 변경
+        self.colorView.backgroundColor = UIColor.blue
+        print("색상 변경 버튼이 클릭 되었음.")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
